475df7ea09698f00c2f8a491ee1ca095373f9a67
Sandalu Pabasara <sandalu.pabasara@racingandsports.com>
2025-11-19 11:07:46 +0530

    feat: Add reduced RL action space definition, comprehensive project documentation, and a 5G use cases reference.

--- algorithms/rl/define_action_space_reduced.m
+++ algorithms/rl/define_action_space_reduced.m
@@ -2,7 +2,7 @@ function actions = define_action_space_reduced()
     handover_thresholds = [0.10, 0.12, 0.15];
     wifi_ratios = [0.60, 0.70];
     weight_max_values = [6, 8];
-    allocation_methods = {'wwa', 'proportional', 'proportional'};
+    allocation_methods = {'wwa', 'wwa', 'wwa'};
     qos_presets = {
         [0.40, 0.30, 0.20, 0.10],
         [0.35, 0.35, 0.20, 0.10]
